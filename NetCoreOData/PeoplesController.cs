23e56877f5912bc125738d419156de0672887794
Murat ÖNER <muhackgames@gmail.com>
2017-12-26 11:05:07 +0300

    - Added .NetCoreOData sample project.(This version is still in test phase.)

---
+++ NetCoreOData/PeoplesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData;
+using Microsoft.AspNetCore.OData.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oData
+{
+
+    public interface IPeopleService
+    {
+        IEnumerable<Person> Get();
+    }
+
+    public interface ITripsService
+    {
+        IEnumerable<Trip> Get();
+    }
+
+    [EnableQuery]
+    [ODataRoute("Peoples")]
+    public class PeoplesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return DemoDataSources.Instance.People.AsQueryable();
+        }
+
+        [ODataRoute("({key})")]
+        public IActionResult GetEntity(int key)
+        {
+            var entity = DemoDataSources.Instance.People.FirstOrDefault(c => c.ID == key);
+            return entity == null ? (IActionResult)NotFound() : new ObjectResult(entity);
+        }
+
+        [ODataRoute("({key})/Trips")]
+        public IActionResult GetTrips(int key)
+        {
+            var entity = DemoDataSources.Instance.People.FirstOrDefault(c => c.ID == key);
+            return entity == null ? (IActionResult)NotFound() : new ObjectResult(entity.Trips);
+        }
+    }
+
+    [EnableQuery]
+    [ODataRoute("Trips")]
+    public class TripsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Trip> Get()
+        {
+            return DemoDataSources.Instance.Trips.AsQueryable();
+        }
+
+        [ODataRoute("({key})")]
+        public IActionResult GetEntity(int key)
+        {
+            var entity = DemoDataSources.Instance.Trips.FirstOrDefault(c => c.ID == key);
+            return entity == null ? (IActionResult)NotFound() : new ObjectResult(entity);
+        }
+
+    }
+
+}
